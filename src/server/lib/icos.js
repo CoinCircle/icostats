2ffb90cbd43842deb80539b2d7b96eaff43cd6f8
Cooper Maruyama <cooperm@susteen.com>
2017-05-21 06:33:12 -0700

    Support more fields on ico models.

--- src/server/lib/icos.js
+++ src/server/lib/icos.js
@@ -17,6 +17,24 @@ function roiSinceICO(ico) {
   return diff;
 }
 
+function roiSinceICOEth(ico, ethPrice) {
+  const exchangeRateLaunch = ico.implied_token_price / ico.eth_price_at_launch;
+  const exchangeRate = ico.price_usd / ethPrice;
+  const roi = exchangeRate / exchangeRateLaunch;
+  const diff = roi - 1;
+
+  return diff;
+}
+
+function roiSinceICOBtc(ico, btcPrice) {
+  const exchangeRateLaunch = ico.implied_token_price / ico.btc_price_at_launch;
+  const exchangeRate = ico.price_usd / btcPrice;
+  const roi = exchangeRate / exchangeRateLaunch;
+  const diff = roi - 1;
+
+  return diff;
+}
+
 /**
  * Return the average ROI per some time period.
  * Examples:
@@ -43,8 +61,22 @@ function roiVsEth(ico, ethPrice) {
   return diff - ethDiff;
 }
 
+function roiVsBtc(ico, btcPrice) {
+  const diff = roiSinceICO(ico);
+  const btcDiff = btcROISinceICO(ico, btcPrice);
+
+  return diff - btcDiff;
+}
+
 function ethROISinceICO(ico, ethPrice) {
   const roi = ethPrice / ico.eth_price_at_launch;
+  const diff = roi - 1;
+
+  return diff;
+}
+
+function btcROISinceICO(ico, btcPrice) {
+  const roi = btcPrice / ico.btc_price_at_launch;
   const diff = roi - 1;
 
   return diff;
@@ -55,13 +87,19 @@ function ethROISinceICO(ico, ethPrice) {
  * @param {Object} ico object as returned from coinmarketcap
  * @return {Object} Normalized object
  */
-export const normalize = (ico, ethPrice) => ({
+export const normalize = (ico, ethPrice, btcPrice) => ({
   ...ico,
   volume_usd_24h: ico['24h_volume_usd'],
+  eth_price_usd: ethPrice,
+  btc_price_usd: btcPrice,
   roi_since_ico: roiSinceICO(ico),
+  roi_since_ico_eth: roiSinceICOEth(ico, ethPrice),
+  roi_since_ico_btc: roiSinceICOBtc(ico, btcPrice),
   roi_per_week: roiPerDays(ico, WEEK),
   roi_per_day: roiPerDays(ico, DAY),
   roi_per_month: roiPerDays(ico, MONTH),
   eth_roi_during_period: ethROISinceICO(ico, ethPrice),
-  roi_vs_eth: roiVsEth(ico, ethPrice)
+  btc_roi_during_period: btcROISinceICO(ico, btcPrice),
+  roi_vs_eth: roiVsEth(ico, ethPrice),
+  roi_vs_btc: roiVsBtc(ico, btcPrice)
 });
