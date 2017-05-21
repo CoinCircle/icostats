33ba49fec24003491eb0eff23ddcfa87dd85aca2
Cooper Maruyama <cooperm@susteen.com>
2017-05-20 18:12:08 -0700

    Ignore env file.

--- settings.js
+++ settings.js
@@ -2,8 +2,10 @@
 require('dotenv').config();
 
 module.exports = {
-    APP_ROOT: __dirname,
-    APP_PORT: process.env.PORT || 3000,
-    MONGO_URI: process.env.MONGO_URI || 'mongodb://mongo/app',
-    MONGO_TEST_URI: process.env.MONGO_TEST_URI || 'mongodb://mongo/app_test'
+  APP_ROOT: __dirname,
+  APP_PORT: process.env.PORT || 3000,
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://mongo/app',
+  MONGO_TEST_URI: process.env.MONGO_TEST_URI || 'mongodb://mongo/app_test',
+  MONGO_USERNAME: process.env.MONGO_USERNAME,
+  MONGO_PASSWORD: process.env.MONGO_PASSWORD
 };
