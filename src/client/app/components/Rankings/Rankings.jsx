5a1fcf74c402491e3937540f4910fa9ea60b7073
Cooper Maruyama <cooperm@susteen.com>
2017-05-22 16:37:57 -0700

    Cleanup.

--- src/client/app/components/Rankings/Rankings.jsx
+++ src/client/app/components/Rankings/Rankings.jsx
@@ -127,27 +127,6 @@ class Rankings extends React.Component {
     };
   }
 
-  handleTableRef(ref) {
-    // // Add the event listener which gets triggered when using the trackpad
-    // ref.addEventListener('mousewheel', function(event) {
-    //   // We don't want to scroll below zero or above the width and height
-    //   var maxX = this.scrollWidth - this.offsetWidth;
-    //   var maxY = this.scrollHeight - this.offsetHeight;
-    //
-    //   // If this event looks like it will scroll beyond the bounds of the element, prevent it and set the scroll to the boundary manually
-    //   if (this.scrollLeft + event.deltaX < 0 ||
-    //     this.scrollLeft + event.deltaX > maxX ||
-    //     this.scrollTop + event.deltaY < 0 ||
-    //     this.scrollTop + event.deltaY > maxY) {
-    //
-    //       event.preventDefault();
-    //
-    //       // Manually set the scroll to the boundary
-    //       this.scrollLeft = Math.max(0, Math.min(maxX, this.scrollLeft + event.deltaX));
-    //       this.scrollTop = Math.max(0, Math.min(maxY, this.scrollTop + event.deltaY));
-    //     }
-    //   }, false);
-  }
 
   getType() {
     const { match: { path }} = this.props;
@@ -268,7 +247,7 @@ class Rankings extends React.Component {
     return (
       <div className={classes.container}>
         {header}
-        <div className={classes.tbody} ref={this.handleTableRef}>
+        <div className={classes.tbody}>
           <Header
             sortBy={this.state.sortBy}
             onSort={(sortBy, ascending) => this.setState({ sortBy, ascending })}
