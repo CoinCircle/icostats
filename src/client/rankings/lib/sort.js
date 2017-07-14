330b5afb4a4f7ee39f04b40c78c3caab4754db59
Cooper Maruyama <me@cooperm.com>
2017-07-14 07:52:00 -0700

    Fix(sorting): Current price sort doesn't work

--- src/client/rankings/lib/sort.js
+++ src/client/rankings/lib/sort.js
@@ -1,5 +1,11 @@
+/**
+ * Logic for sorting. To be used as a callback for Array.prototype.sort
+ * @flow
+ */
+/* eslint-disable max-params, complexity, max-statements */
 import * as utils from '~/app/utils';
 import moment from 'moment';
+import type { $ICO, $Currency } from 'shared/types.flow';
 
 /**
  * Handles sorting logic for ranking.
@@ -9,9 +15,15 @@ import moment from 'moment';
  * @param {Boolean} ascending
  * @return {Number}
  */
-export default function handleSort(_a, _b, sortBy, ascending, currency, startDate) {
-  let a = _a[sortBy];
-  let b = _b[sortBy];
+export default function handleSort(
+_a: $ICO,
+_b: $ICO,
+sortBy: string,
+ascending: boolean,
+currency: $Currency
+) {
+  let a = _a[sortBy] || _a;
+  let b = _b[sortBy] || _b;
 
   if (sortBy.includes('.')) {
     a = sortBy.split('.').reduce((p, c) => p[c], _a);
@@ -22,8 +34,8 @@ export default function handleSort(_a, _b, sortBy, ascending, currency, startDat
     case 'name': {
       a = a.toLowerCase();
       b = b.toLowerCase();
-      if (a < b) return ascending ? 1 : -1;
-      if (a > b) return ascending ? -1 : 1;
+      if (a < b) return ascending ? -1 : 1;
+      if (a > b) return ascending ? 1 : -1;
       return 0;
     }
     case 'start_date': {
@@ -31,8 +43,8 @@ export default function handleSort(_a, _b, sortBy, ascending, currency, startDat
       const ma = moment(a, format);
       const mb = moment(b, format);
 
-      if (ma < mb) return ascending ? 1 : -1;
-      if (ma > mb) return ascending ? -1 : 1;
+      if (ma < mb) return ascending ? -1 : 1;
+      if (ma > mb) return ascending ? 1 : -1;
       return 0;
     }
     case 'ico_price': {
