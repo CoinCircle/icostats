c5ce14bd60170b1deca45a673d3fd507129b2e7a
Cooper Maruyama <me@cooperm.com>
2017-08-13 15:01:44 -0700

    :bug: Fix crashing production build

--- scripts/webpack/webpack.config.prod.js
+++ scripts/webpack/webpack.config.prod.js
@@ -1,29 +1,29 @@
 const webpack = require('webpack');
-const BabiliPlugin = require("babili-webpack-plugin");
+const BabiliPlugin = require('babili-webpack-plugin');
 const clientBase = require('./base/client.base.config.js');
 const serverBase = require('./base/server.base.config.js');
 const path = require('path');
 const ROOT_DIR = path.resolve(__dirname, '../../');
 
-const client = Object.assign({}, client, {
-  plugins: client.plugins.concat([
+const client = Object.assign({}, clientBase, {
+  plugins: clientBase.plugins.concat([
     new webpack.DefinePlugin({
       ROOT_DIR: JSON.stringify(ROOT_DIR),
       DEBUG: JSON.stringify(false)
     }),
-    new BabiliPlugin(babiliOptions, overrides)
+    new BabiliPlugin()
   ])
 });
 
-const server = Object.assign({}, server, {
-  plugins: server.plugins.concat([
+const server = Object.assign({}, serverBase, {
+  plugins: serverBase.plugins.concat([
     new webpack.DefinePlugin({
       ROOT_DIR: JSON.stringify(ROOT_DIR),
       DEBUG: JSON.stringify(false)
     }),
-    new BabiliPlugin(babiliOptions, overrides)
+    new BabiliPlugin()
   ])
-})
+});
 
 module.exports = [
   client,
